62f7828e517c2120bed60706661efe9e7eca98b8
Nodjo <noddjo@gmail.com>
2026-03-23 23:02:20 +0100

    Fix production warning when composer install isn't run

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,25 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+declare(strict_types=1);
+
+require_once __DIR__ . '/stubs/BgaFrameworkStubs.php';
+
+$autoload = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoload)) {
+    require_once $autoload;
+}
+
+spl_autoload_register(static function (string $class): void {
+    $prefix = 'Bga\\Games\\DiceForge\\';
+
+    if (!str_starts_with($class, $prefix)) {
+        return;
+    }
+
+    $relativeClass = substr($class, strlen($prefix));
+    $path = __DIR__ . '/../modules/php/' . str_replace('\\', '/', $relativeClass) . '.php';
+
+    if (file_exists($path)) {
+        require_once $path;
+    }
+});
